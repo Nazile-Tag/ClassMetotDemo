433ff7b53aad661bfbf08d91f80bb5d6d558b6e3
Nazile-Tag <tagnazile@gmail.com>
2021-01-19 21:03:17 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Eklendi" + musteri.Id + musteri.Adi + musteri.Soyadi + musteri.Sehir);
+        
+        }
+
+        public void Sil(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Silindi" );
+        }
+    }
+    
+}   
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 223344550;
+            musteri1.Adi = "Murat";
+            musteri1.Soyadi = "Tağ";
+            musteri1.Sehir = "Mersin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 112233445;
+            musteri2.Adi = "Nazile";
+            musteri2.Soyadi = "Tağ";
+            musteri2.Sehir = "Ankara";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 112256788;
+            musteri3.Adi = "Ercan";
+            musteri3.Soyadi = "Uysal";
+            musteri3.Sehir = "Tokat";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 112256789;
+            musteri4.Adi = "Feray";
+            musteri4.Soyadi = "Uysal";
+            musteri4.Sehir = "Bartın";
+
+            Musteri musteri5 = new Musteri();
+            musteri5.Id = 134569999;
+            musteri5.Adi = "Akın Sercan";
+            musteri5.Soyadi = "Budak";
+            musteri5.Sehir = "Ankara";
+
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4, musteri5 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + musteri.Adi + musteri.Soyadi + musteri.Sehir);
+            }
+
+
+
+
+        }
+    }
+}
